ca4b65253fa9f612a7f5e940f6acda1680ba629b
fzzyhmstrs <fzzyhmstrs@gmail.com>
2023-06-17 09:41:45 -0400

    fix mixin

--- src/main/java/fzzyhmstrs/lootify/mixins/LootManagerMixin.java
+++ src/main/java/fzzyhmstrs/lootify/mixins/LootManagerMixin.java
@@ -4,28 +4,32 @@ import com.google.gson.JsonElement;
 import fzzyhmstrs.lootify.server.ServerResourceData;
 import net.minecraft.loot.LootManager;
 import net.minecraft.resource.ResourceManager;
+import net.minecraft.resource.ResourceReloader;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.profiler.Profiler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 
-@Mixin(value = LootManager.class, priority = 3000)
+@Mixin(value = LootManager.class, priority = 2500)
 public class LootManagerMixin {
 
-    @Inject(method = "apply(Ljava/util/Map;Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/util/profiler/Profiler;)V", at = @At("HEAD"))
-    private void lootify_loadLootifyLootTables(Map<Identifier, JsonElement> map, ResourceManager resourceManager, Profiler profiler, CallbackInfo ci){
+    @Inject(method = "reload", at = @At("HEAD"))
+    private void lootify_loadLootifyLootTables(ResourceReloader.Synchronizer synchronizer, ResourceManager manager, Profiler prepareProfiler, Profiler applyProfiler, Executor prepareExecutor, Executor applyExecutor, CallbackInfoReturnable<CompletableFuture<Void>> cir){
         ServerResourceData.initLootifyTableLoading();
-        ServerResourceData.loadLootifyTables(resourceManager);
+        ServerResourceData.loadLootifyTables(manager);
     }
 
-    @Inject(method = "apply(Ljava/util/Map;Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/util/profiler/Profiler;)V", at = @At("RETURN"))
-    private void lootify_finishApplyingLootifyLootTables(Map<Identifier, JsonElement> map, ResourceManager resourceManager, Profiler profiler, CallbackInfo ci){
-
+    @Inject(method = "reload", at = @At("RETURN"), cancellable = true)
+    private void lootify_finishApplyingLootifyLootTables(ResourceReloader.Synchronizer synchronizer, ResourceManager manager, Profiler prepareProfiler, Profiler applyProfiler, Executor prepareExecutor, Executor applyExecutor, CallbackInfoReturnable<CompletableFuture<Void>> cir){
         ServerResourceData.logMissedLootifyTables();
+        cir.setReturnValue(cir.getReturnValue());
     }
 
 }
